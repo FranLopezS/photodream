b149a09cddf9481d550d60ac2d9fa546d5771ae5
Fran <franlopezsanchez97@gmail.com>
2024-11-03 17:03:29 +0100

    favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href={{ asset('favicon.ico') }} />
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <meta name="title" content="Photodream" />
